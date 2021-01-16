c8b23eac16d7f06f50956a56d8356e0adbd06fb3
ChasTechProjects <44618658+ChasTechProjects@users.noreply.github.com>
2021-01-16 11:49:43 +0000

    Adapt for AOSP

--- aosp_star2lte.mk
+++ aosp_star2lte.mk
@@ -24,11 +24,11 @@ $(call inherit-product, device/samsung/star2lte/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := star2lte
-PRODUCT_NAME := lineage_star2lte
+PRODUCT_NAME := aosp_star2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G965F
 PRODUCT_MANUFACTURER := samsung
